5dd39330b0ac0e0b78f774ee93d6232ac010d1e9
Santiago Rodriguez <santiara010@gmail.com>
2024-08-01 00:07:10 -0300

    Hice el punto 1 sin problemas

--- js/Pto-1.js
+++ js/Pto-1.js
@@ -1,1 +1,11 @@
 
+const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+
+document.write(
+    `<h1>Lista de meses:</h1>`
+  );
+  document.write(`<ul>`);
+  for (let i = 0; i < meses.length; i++) {
+    document.write(`<li>${meses[i]}</li>`);
+  }
+  document.write(`</ul>`);
