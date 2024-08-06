2238307eafae807dfd457f10dff8d34e10229723
Santiago Rodriguez <santiara010@gmail.com>
2024-08-05 23:17:39 -0300

    Termine el punto 5 con ayuda de los ejercicios resueltos, para detectar algunos errores

--- js/Pto-5.js
+++ js/Pto-5.js
@@ -1,1 +1,18 @@
+/* 5- Definir una función que muestre información sobre una cadena de texto que se
+le pasa como argumento. A partir de la cadena que se le pasa, la función
+determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o
+por una mezcla de ambas. */
+function analisisArgumento(cadenaTexto){
+    if(cadenaTexto === cadenaTexto.toUpperCase()){
+        return ("Este argumento contiene solo mayusculas")
+    } else
+    if(cadenaTexto === cadenaTexto.toLowerCase()){
+        return ("Este argumento contiene solo minusculas")
+    } else {
+        return ("Este argumento contiene mezcla de minuscula y mayuscula")
+    }
+}
+const cadenaTexto = prompt("Ingrese un argumento");
 
+
+document.write (`${analisisArgumento(cadenaTexto)}`)
